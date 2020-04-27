3d3fcee90d83aae5955b401188201e34281d24bf
Trevor Davis <trevormckay.davis@gmail.com>
2020-04-27 07:14:10 -0500

    fixed xml loader issued and uniquified the units list

--- src/card.js
+++ src/card.js
@@ -1,23 +1,23 @@
 import React from "react";
 
 // import cat from "xml-loader!./test_data/Aeldari - Harlequins.cat";
-import ros from "xml-loader!./test_data/Z - HQ 2000.ros";
+import ros from "./test_data/Z - HQ 2000.ros";
 // import ros from "xml-loader!./test_data/AC 2000 all armor.ros";
 
 function listSelections() {
   // console.log(cat.catalogue.publications[0].publication[0].$.name);
   // console.log(ros.roster.forces[0].force[0].selections[0].selection);
-  const list = ros.roster.forces[0].force.reduce((acc, force) => {
-    return acc.concat(
-      force.selections[0].selection.map(selection => {
-        if (selection.$.type === "unit") {
-          return selection.$.name;
-        }
-      })
+  const list = ros.roster.forces[0].force.reduce((forceAcc, force) => {
+    return forceAcc.concat(
+      force.selections[0].selection.reduce((selectionAcc, selection) => {
+        return selection.$.type === "unit"
+          ? [...selectionAcc, selection.$.name]
+          : selectionAcc;
+      }, [])
     );
   }, []);
-  const uniqueList = list.reduce((acc, item) => {
-    acc.includes(item) ? acc : [...acc, item];
+  const uniqueList = list.reduce((acc, item, idx) => {
+    return acc.includes(item) ? acc : [...acc, item];
   }, []);
   return uniqueList;
   s;
@@ -28,8 +28,8 @@ export default function Card(props) {
   return (
     <div>
       CARDS
-      {list.map(item => (
-        <div>{item}</div>
+      {list.map((item, idx) => (
+        <div key={idx}>{item}</div>
       ))}
     </div>
   );
